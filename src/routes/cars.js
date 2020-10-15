110bdb3858f0e1ae1d67afb53abc798d493df9a1
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-15 14:52:11 +0100

    feat: Added update car endpoint

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -12,30 +12,50 @@ router.post('/', (req, res) => {
     //check that the request body has all the required parameters
     let carHasAllProperties = (newCar.id && newCar.make && newCar.model && newCar.colour && newCar.year);
     //check if this car already exists in the json file
-    let carExists = cars.some(car => 
-        car.make === newCar.make && 
-        car.model == newCar.model && 
-        car.colour === newCar.colour && 
+    let carExists = cars.some(car =>
+        car.make === newCar.make &&
+        car.model == newCar.model &&
+        car.colour === newCar.colour &&
         car.year === newCar.year);
 
     if (carHasAllProperties && !carExists) {
         cars.push(newCar);
         res.json(newCar);
     } else {
-        res.status(500).json({error: 'Error adding this car'});
+        res.status(500).json({ error: 'Error adding this car' });
     }
 });
 
 //remove a car
-router.delete('/:id', (req,res) => {
+router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    
+
     cars.forEach((car, index) => {
-        if(car.id === id){
+        if (car.id === id) {
             cars.splice(index, 1);
             res.json(cars);
         }
     });
 });
+
+//update a car entry
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { make, model, colour, year } = req.body;
+
+    if(id && make && model && colour && year){
+        cars.forEach(car => {
+            if(car.id === id){
+                car.make = make;
+                car.model = model;
+                car.colour = colour;
+                car.year = year;
+            }
+        });
+        res.json({ id, make, model, colour, year });
+    } else {
+        res.status(500).json({ error: 'Error updating this car'});
+    }
+})
 
 module.exports = router
