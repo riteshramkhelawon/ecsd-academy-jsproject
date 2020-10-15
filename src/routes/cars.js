0f918f5db04131144e10221bc691c3ef1e974750
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-15 14:27:38 +0100

    feat: Added add car endpoint

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -6,4 +6,24 @@ const cars = require('../sample.json')
 //retrieve all cars
 router.get('/', (req, res) => res.json(cars))
 
+//add a new car
+router.post('/', (req, res) => {
+    const newCar = { id: (cars.length + 1).toString(), ...req.body }
+    //check that the request body has all the required parameters
+    let carHasAllProperties = (newCar.id && newCar.make && newCar.model && newCar.colour && newCar.year);
+    //check if this car already exists in the json file
+    let carExists = cars.some(car => 
+        car.make === newCar.make && 
+        car.model == newCar.model && 
+        car.colour === newCar.colour && 
+        car.year === newCar.year);
+
+    if (carHasAllProperties && !carExists) {
+        cars.push(newCar);
+        res.json(newCar);
+    } else {
+        res.status(500).json({error: 'Error adding this car'});
+    }
+});
+
 module.exports = router
