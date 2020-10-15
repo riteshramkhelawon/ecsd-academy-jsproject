bb0880473b41d7fffd1987306b2fad5c6400a53b
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-15 12:31:36 +0100

    chore: Changed app.js to reference /api/cars

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ app.set('port', process.env.PORT || 4000)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use('/api/movies', require('./routes/movies'))
+app.use('/api/cars', require('./routes/cars'))
 
-app.listen(app.get('port'), () => console.log(`Server on port ${app.get('port')}`))
+app.listen(app.get('port'), () => console.log(`Server listening on port ${app.get('port')}...`))
